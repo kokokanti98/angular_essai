2e0e2e743edf46fe659ba577834b17d53f56450d
KOKO Ambinintsoa Kantisambatra <45491492+kokokanti98@users.noreply.github.com>
2021-03-23 11:28:05 +0100

    Filtered Hotels
    Ajout des methode necessaire pour faire le filtre des hotels par nom sur le input un genre de recherche

--- src/app/hotel-list/hotel-list.component.ts
+++ src/app/hotel-list/hotel-list.component.ts
@@ -7,11 +7,46 @@ import { IHotel } from './hotel';
   templateUrl: './hotel-list.component.html',
   styleUrls: ['./hotel-list.component.css']
 })
-export class HotelListComponent  implements OnInit{
+export class HotelListComponent implements OnInit{
+  //Le variable hotelFilter
+  private _hotelFilter = "mot";
+
+  public filteredHotels: IHotel[] = [];
+  //getteur hotelFilter
+  public get hotelFilter(): string {
+    return this._hotelFilter;
+  }
+  //setteur hotelFilter
+  public set hotelFilter(filter: string) {
+    this._hotelFilter = filter;
+    //Operation ternaires
+    //Si la nouvelle liste d hotel filteredHotels recoit une nouvelle valeur hotelFilter alors on lance filterHotels et on y passe la valeur hotelFilter
+    //Sinon retourne la liste complète des hotels
+    this.filteredHotels = this.hotelFilter ? this.filterHotels(this.hotelFilter) : this.hotels;
+  }
+
+  //methode qui aide a faire le filtre de la liste
+  private filterHotels(criteria: string): IHotel[] {
+    //met en minuscule
+    criteria = criteria.toLocaleLowerCase();
+    //Pour chaque hotel dans IHotel ici hotels est la listes des hotels
+    //pour chq hotel de type IHotel qu'on va parcourir on va chercher et mettre hotelName en minuscule
+    //l index de ce mot la comparer avec le nom de l hotel est que cela nous donne -1 alors on peut retourner la nouvelle liste car index >= 0
+    const res = this.hotels.filter(
+      (hotel: IHotel) => hotel.hotelName.toLocaleLowerCase().indexOf(criteria) !== -1
+    );
+
+    return res;
+
+  }
+
   ngOnInit(): void {
     console.log("Mon niveau de vie initial fonctionne");
+    //on initialise la valeur de filterHotels par la collection de données prise hotels
+    this.filteredHotels = this.hotels;
   }
   public title = 'Liste d\'hotel';
+
   public hotels: IHotel[] = [
     {
       "hotelId": 1,
@@ -56,8 +91,7 @@ export class HotelListComponent  implements OnInit{
   ];
   //Le variable boolean par défaut il est FALSE
   public showBadge: boolean;
-  //Le variable hotelFilter
-  public hotelFilter = "mot";
+
   //Une event qui retourne rien ou void
   public toggleIsNewBadge(): void {
     //ICI il va nous retourner le contraire si c'est TRUE va nous retourner FALSE et vice versa
--- src/app/hotel-list/hotel.ts
+++ src/app/hotel-list/hotel.ts
@@ -15,15 +15,29 @@ export class Hotel implements IHotel {
     public description: string,
     public price: number,
     public imageUrl: string,
-  ) {}
-  //Fonction GetPrice ou getteur de la variable price qui bien sur retourne un number methode 1
-  getNewPrice1(p_price: number): number {
+  ) { }
+  //Pour les variables privées ils sont déclarées ainsi:
+  //return this._price = p_price;
+  //return this._hotelName = p_hotelName + ' ';
+
+  //Fonction SetPrice ou setteur de la variable price qui bien sur retourne un number
+  setNewPrice(p_price: number): number {
     //affecter la valeur aux parametre price de la clase grâce aux paramètres de la fonction p_price
     return this.price = p_price;
   }
-  //Fonction GetPrice ou getteur de la variable price qui bien sur retourne un number autres methodes
+  //Fonction GetPrice ou getteur de la variable price qui bien sur retourne un number
   getNewPrice(price: number): number {
     //retourne le prix - 5
     return price - 5;
+  }
+  //Fonction SetPrice ou setteur de la variable hotelName qui bien sur retourne un String
+  setHotelName(p_hotelName: number): string {
+    //affecter la valeur aux parametre price de la clase grâce aux paramètres de la fonction p_HotelName et on ajoute le ' ' à la fin
+    return this.hotelName = p_hotelName + ' ';
+  }
+  //Fonction GetPrice ou getteur de la variable hotelName qui bien sur retourne un String
+  getHotelName(): string {
+    //retourne le nom de l hotel
+    return this.hotelName;
   }
 }
